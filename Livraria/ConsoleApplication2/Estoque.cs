d66d599d98c458d2d18af676db8cf607bb0955f7
Davi Mattos <davimattos2012@gmail.com>
2016-07-19 22:41:55 -0300

    trab

---
+++ Livraria/ConsoleApplication2/Estoque.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Estoque
+    {
+        public string nomeLivro { get; set; }
+        public int isbnLivro { get; set; }
+        public void Cadastrar()
+        {
+            Console.Clear();
+            Console.Write("Nome: ");
+            nomeLivro = Console.ReadLine();
+            Console.Write("ISBN: ");
+            isbnLivro = int.Parse(Console.ReadLine());
+        }
+        public void Consultar()
+        {
+
+        }
+        public void Alterar()
+        {
+
+        }
+        public void Remover()
+        {
+
+        }
+    }
+}
