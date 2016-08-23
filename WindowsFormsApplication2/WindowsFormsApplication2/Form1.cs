e2a0f0c24f416e6ca4e676c09c12551ee87bbf16
Davi Mattos <davimattos2012@gmail.com>
2016-08-23 15:53:20 -0300

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class FrmCadastrar : Form
+    {
+        Jogo j = new Jogo();
+        Gerenciador g = new Gerenciador();
+        int count = 0;
+        public FrmCadastrar()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            count++;
+            j.Nome = txtNome.Text;
+            j.Genero = txtGenero.Text;
+            j.Codigo = count;
+            txtNome.Clear();
+            txtGenero.Clear();
+            g.AdicionarJogo(j);
+            lblQntd.Text = "Itens Cadastrados: " + count.ToString(); 
+            
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(g.GerarDadosLista());
+        }
+
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Jogo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Jogo
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public int Codigo { get; set; }
+
+    }
+}
