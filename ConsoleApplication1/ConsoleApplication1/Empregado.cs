49458ce0afb9367ab92e86717f0c077689b061d6
Davi Mattos <davimattos2012@gmail.com>
2016-09-20 14:31:54 -0300

    Herança

---
+++ ConsoleApplication1/ConsoleApplication1/Empregado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Empregado:Funcionario
+    {
+        public double VR { get; set; }
+        public double VT { get; set; }
+
+        public Empregado(double _vr, string _nome, string _cpf):base(_nome, _cpf)
+        {
+            VR = _vr; 
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Terceirizado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Terceirizado : Empregado
+    {
+        public int Dependentes { get; set; }
+        public Terceirizado(double _vr, string _nome, string _cpf) : base(_vr, _nome, _cpf)
+        { }
+    }
+}
