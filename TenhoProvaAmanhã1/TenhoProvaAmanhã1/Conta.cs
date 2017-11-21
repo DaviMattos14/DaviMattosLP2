9c2f7776f22823a4a335a30e2f73c98707ee4db4
DaviMattos14 <davimattos2012@gmail.com>
2017-11-20 23:07:02 -0200

    21-11-17

---
+++ TenhoProvaAmanhã1/TenhoProvaAmanhã1/Conta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenhoProvaAmanhã1
+{
+    class Conta
+    {
+        public int NumeroDaConta { get; set; }
+        public string Titular { get; set; } 
+        public double Saldo { get; set; }
+
+        public bool Saque (double valor)
+        {
+            if (valor < Saldo)
+            {
+                Saldo -= valor;
+                return true;
+            }
+            return false;
+        }
+        public void Deposito (double valorDeposito)
+        {
+                Saldo += valorDeposito;
+        }
+    }
+}
---
+++ TenhoProvaAmanhã1/TenhoProvaAmanhã1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenhoProvaAmanhã1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            List<Conta> contas = new List<Conta>();
+            Console.Write("Contas a cadastrar: ");
+            int qtdContas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdContas; i++)
+            {
+                Console.Write("Numero da Conta: ");
+                conta.NumeroDaConta = int.Parse(Console.ReadLine());
+                Console.Write("Nome do titular: ");
+                conta.Titular = Console.ReadLine();
+                Console.Write("Saldo: ");
+                conta.Saldo = double.Parse(Console.ReadLine());
+                contas.Add(conta);
+            }
+
+            Console.Write("Nº da conta para consulta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            int j = 0;
+            bool aux = true;
+            while(aux)
+            {
+                if (numeroConta == contas[j].NumeroDaConta)
+                {
+                    Console.Write("Valor de Deposito: ");
+                    double deposito = double.Parse(Console.ReadLine());
+                    conta.Deposito(deposito);
+
+                    Console.Write("Valor de Saque: ");
+                    double valorSaque = double.Parse(Console.ReadLine());
+
+                    bool saque = conta.Saque(valorSaque);
+                    if (saque == true) Console.WriteLine("Saque Realizado com Sucesso");
+                    else Console.WriteLine("Saque Falhou");
+
+                    Console.WriteLine("Conta: {0} Titular: {1} Saldo: {2,8:c}", contas[j].NumeroDaConta, contas[j].Titular, contas[j].Saldo);
+                    aux = false;
+                }
+                else //if (j > qtdContas)
+                {
+                    Console.WriteLine("Error");
+                    aux = false;
+                }
+                j++;
+            }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
